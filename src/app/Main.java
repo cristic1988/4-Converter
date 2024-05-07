4bf790b296c3c8abcdfe249814ce919ddb14fcee
cristic1988 <cristic8@gmail.com>
2024-05-07 22:38:50 +0300

    Add ml to kilometres convert

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,9 +2,17 @@ package app;
 
 public class Main {
 
+    private static final double CONV_K = 1.609344;
+
     public static void main(String[] args) {
         System.out.println("App for converting units of length.");
+        double ml = 3;
+        double kilometres = convMlToKm(ml);
+        System.out.println("Result is " + kilometres + " km.");
 
+    }
+    private static double convMlToKm(double ml) {
+        return ml * CONV_K;
     }
 
 }
